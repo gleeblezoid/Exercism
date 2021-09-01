7135c9240e381781cea147b56499c5f9cb08aebe
gleeblezoid <gleeblezoid@github.com>
2021-09-01 11:36:51 +0100

    Tried again

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -1,24 +1,30 @@
 package tree
 
+// Record type for tree input
 type Record struct {
 	ID     int
 	Parent int
 }
 
+// Node type for tree output
 type Node struct {
 	ID       int
 	Children []*Node
 }
 
+// Build records function
 func Build(records []Record) (*Node, error) {
 	// Nodes have an ID (the parent ID) and then a slice of pointers to nodes
 	// We need to go through the slice of records and make a node which takes the parent ID as an int key and the ID as a pointer which is put into a slice
-	var n Node
+	var n *Node
+	var c []*Node
 
 	for _, r := range records {
 		n.ID = r.Parent
-		childID := []&r.ID
-		n.Children = append(n.Children, childID)
+		c[0].ID = r.ID
+		c[0].Children = nil
+		n.Children = c
+
 	}
 	return n, nil
 }
